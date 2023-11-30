cb1c4f2e7a627ea4f69738aa1cbcaa2d34468bc4
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-11-30 14:46:12 -0300

    adicionando endpoint para upload de arquivos

--- Application/Controllers/UserController.cs
+++ Application/Controllers/UserController.cs
@@ -1,8 +1,11 @@
+using Azure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Trainify.Me_Api.Application.Controllers.Shared;
 using Trainify.Me_Api.Application.Models.Requests;
 using Trainify.Me_Api.Domain.Entities;
+using Trainify.Me_Api.Infra.Services.BlobStorage;
 
 namespace Trainify.Me_Api.Application.Controllers
 {
@@ -38,5 +41,19 @@ namespace Trainify.Me_Api.Application.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<ActionResult<string>> UploadFile([FromBody] UploadFileRequest fileRequest)
+        {
+            try
+            {
+                var data = new MemoryStream(Convert.FromBase64String(fileRequest.Base64Arquivo));
+                var urlDoArquivo = await BlobStorage.UploadFile(fileRequest.NomeArquivo, fileRequest.ExtensaoArquivo, fileRequest.MimeType, data);
+                return StatusCode(200, urlDoArquivo);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
